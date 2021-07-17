595f1437e78b2ab16d80f60b9d2e26de85a53766
Dru Ludwig <druludwig@yahoo.com>
2021-07-16 19:26:00 -0700

    Testing anchors in live environment

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 let licenseURL;
+let licenseBadge;
 
 // Page content
 const generateREADME = (answers) =>
   `# ${answers.projectName}
-  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-  Launch App: <a href="${answers.deployedURL}">${answers.deployedURL}</a><br />
+  Launch App: <a href="${answers.deployedURL}">${answers.deployedURL}</a> <br />
+  ${licenseURL}<br />
   ## Description<br />
   ${answers.projectDescription}<br />
-  ## Installation Instructions<br />
+  ## Contents
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Instructions](#Instructions)
+  - [Contributions](#Contributions)
+  - [Testing](#Testing)
+  - [License](#License)
+  - [Questions](#Questions?)
+ 
+  ## Installation<br />
   ${answers.projectInstallation}<br />
-  ## Usage Instructions<br />
+  ## Instructions<br />
   ${answers.projectUsage}<br />
   ## Contributions<br />
   ${answers.projectContributions}<br />
@@ -19,6 +29,11 @@ const generateREADME = (answers) =>
   ${answers.projectTesting}<br />
   ## License<br />
   ${answers.projectLicense}<br />
+  ## Questions?<br />
+  - Please email any questions to: <a href="mailto:${answers.email}">${answers.email}</a>
+  - Find me on GitHub: <a href="https://github.com/${answers.github}">${answers.github}</a><br />
+  <br />
+
 `  
 
 // Prompt user for input.
@@ -48,7 +63,7 @@ inquirer.prompt([
     {
       type: 'input',
       name: 'projectDescription',
-      message: 'Describe the project (2-3 sentences):',
+      message: 'Describe the project (Use <br /> for a new line):',
       validate: function (answer) {
         if (answer.length < 1) {
             return console.log("This field is required.");
@@ -59,7 +74,7 @@ inquirer.prompt([
     {
       type: 'input',
       name: 'projectInstallation',
-      message: 'Enter installation instructions:',
+      message: 'Enter installation instructions (Use <br /> for a new line):',
       validate: function (answer) {
         if (answer.length < 1) {
             return console.log("This field is required.");
@@ -70,7 +85,7 @@ inquirer.prompt([
     {
       type: 'input',
       name: 'projectUsage',
-      message: 'Enter instructions for use:',
+      message: 'Enter instructions for use (Use <br /> for a new line):',
       validate: function (answer) {
         if (answer.length < 1) {
             return console.log("This field is required.");
@@ -105,25 +120,46 @@ inquirer.prompt([
       choices: ["MIT License", "Apache License", "Mozilla Public License"],
       name: 'projectLicense',
       message: 'Choose a license:',
-
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter email:',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("This field is required.");
+        }
+        return true;
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter Github username:',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("This field is required.");
+        }
+        return true;
+      }
     },
 ])
 
+// Build page based on responses
     .then((answers) => {
     let licenseMarkup = answers.projectLicense
-    
     if (licenseMarkup == 'MIT License') {
-      answers.projectLicense = `Use of this project is subject to the terms and conditions of the <a href="https://www.mit.edu/~amini/LICENSE.md">MIT License</a>.`
+      answers.projectLicense = `Use of this project is subject to the terms and conditions of the <a href="https://www.mit.edu/~amini/LICENSE.md">MIT License</a>.`;
+      licenseURL = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
     }
     if (licenseMarkup == 'Apache License') {
       answers.projectLicense = `Use of this project is subject to the terms and conditions of the <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache License</a>.`;
-      licenseURL = `https://opensource.org/licenses/Apache-2.0`
+      licenseURL = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
     }
     if (licenseMarkup == 'Mozilla Public License') {
-      answers.projectLicense = `Use of this project is subject to the terms and conditions of the <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache License</a>.`
+      answers.projectLicense = `Use of this project is subject to the terms and conditions of the <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache License</a>.`;
+      licenseURL = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
     }
-
-
 
     const mdPageContent = generateREADME(answers);
     
--- testing/README.md
+++ testing/README.md
@@ -1,14 +1,29 @@
-# WERWER
-  Launch App: <a href="FSDF">FSDF</a><br />
+# wqdsqwdeqw
+  Launch App: <a href="wrtyu">wrtyu</a> <br />
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)<br />
   ## Description<br />
-  A<br />
-  ## Installation Instructions<br />
-  FSD<br />
-  ## Usage Instructions<br />
-  FSF<br />
+  iulouliuk<br />
+  ## Contents
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Instructions](#Instructions)
+  - [Contributions](#Contributions)
+  - [Testing](#Testing)
+  - [License](#License)
+  - [Questions](#Questions?)
+ 
+  ## Installation<br />
+  errwf<br />
+  ## Instructions<br />
+  edew<br />
   ## Contributions<br />
-  ASDF<br />
+  sxasx<br />
   ## Testing<br />
-  SA<br />
+  qweewr<br />
   ## License<br />
   Use of this project is subject to the terms and conditions of the <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache License</a>.<br />
+  ## Questions?<br />
+  - Please email any questions to: <a href="mailto:deewrw4r">deewrw4r</a>
+  - Find me on GitHub: <a href="https://github.com/jkkk">jkkk</a><br />
+  <br />
+
